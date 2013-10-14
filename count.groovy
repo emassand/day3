f0be23ead0f2f076707427e334503ef121598e88
Esha <E.Massand@bbk.ac.uk>
2013-10-14 23:25:01 +0100

    still not finished

---
+++ count.groovy
@@ -0,0 +1,14 @@
+println "Please enter some text so I can play a game with you!"
+String text = System.console().readLine()
+
+char[] letters = text.toCharArray()
+
+int letterCount = 0
+if (letters == "e") {
+  letterCount++;}
+println letterCount
+
+
+
+
+
